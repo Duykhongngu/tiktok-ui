a3779ef0ecde6da454bacb1d43ffe26b3e645b7e
Duykhongngu <nguyenduy15042003vp@gmail.com>
2025-01-13 16:19:50 +0700

    sử dụng thư viện Axios

--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -5,20 +5,24 @@ import { Wrapper as PopperWrapper } from '~/components/Popper';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames/bind';
 import styles from './Search.module.scss';
+import { useDebounce } from '~/hooks';
 import { SearchIcon } from '~/components/Icons';
+import * as searchServices from '~/apiServices/searchServices';
+
+
 import {
     faCircleXmark,
     faSpinner,
 } from '@fortawesome/free-solid-svg-icons';
+const cx = classNames.bind(styles);
 
 
 function Search() {
     const [searchValue, setSearchValue] = useState('');
     const [searchResult, setSearchResult] = useState([]);
-    const cx = classNames.bind(styles);
     const [showResult, setShowResult] = useState(true);
     const [loading, setLoading] = useState(true);
-
+    const debounced = useDebounce(searchValue, 500);
     const inputRef = useRef();
     const handleClear = () => {
 
@@ -31,21 +35,23 @@ function Search() {
         setShowResult(false);
     }
     useEffect(() => {
-        if (!searchValue.trim()) {
+        if (!debounced.trim()) {
             setSearchResult([]);
+            setLoading(false);
             return;
         }
         setLoading(true);
-        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(searchValue)}&type=less`)
-            .then(res => res.json())
-            .then(res => {
-                setSearchResult(res.data);
-                setLoading(false);
-            })
-            .catch(() => {
-                setLoading(false);
-            })
-    }, [searchValue]);
+        const fetchApi = async () => {
+            setLoading(true);
+
+            const result = await searchServices.search(debounced);
+            setSearchResult(result);
+
+            setLoading(false);
+        }
+
+        fetchApi();
+    }, [debounced]);
     return (
         <HeadlessTippy
             interactive

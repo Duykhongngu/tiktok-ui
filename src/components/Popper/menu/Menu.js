66161ccdcd99d44b113086eb038df55346b4cb72
Duykhongngu <nguyenduy15042003vp@gmail.com>
2025-01-14 17:24:10 +0700

    Giải quyết vấn đề nhiều file index.js

---
+++ src/components/Popper/menu/Menu.js
@@ -0,0 +1,69 @@
+import Tippy from '@tippyjs/react/headless';
+import classNames from 'classnames/bind';
+import PropTypes from 'prop-types'
+
+import styles from './Menu.module.scss';
+import MenuItem from './MenuItem';
+import Headers from './Header';
+import { useState } from 'react';
+import { Wrapper as PopperWrapper } from '~/components/Popper';
+
+const defaultFn = () => { };
+const cx = classNames.bind(styles);
+function Menu({ children, items = [], onChange = defaultFn, hideOnClick = false }) {
+    const [history, setHistory] = useState([{ data: items }]);
+    const curent = history[history.length - 1];
+    const renderItems = () => {
+        return curent.data.map((item, index) => {
+            const isParent = !!item.children;
+            return (
+                <MenuItem
+                    key={index}
+                    data={item}
+                    onClick={() => {
+                        if (isParent) {
+                            setHistory((prev) => [...prev, item.children]);
+                        } else {
+                            onChange(item);
+                        }
+                    }}
+                />
+            )
+        }
+
+        );
+    };
+    return (
+        <Tippy
+
+            offset={[12, 8]}
+            hideOnClick={hideOnClick}
+            interactive
+            placement='bottom-end'
+            delay={[0, 500]}
+            render={(attrs) => (
+                <div className={cx('menu-list')} tabIndex="-1" {...attrs}>
+                    <PopperWrapper className={cx('menu-popper')}>
+                        {history.length > 1 &&
+                            <Headers title={curent.title}
+                                onBack={() => setHistory((prev) =>
+                                    prev.slice(0, prev.length - 1))} />
+                        }
+                        <div className={cx('menu-body')}>{renderItems()}</div>
+                    </PopperWrapper>
+                </div>
+            )}
+            onHide={() => setHistory((prev) => prev.slice(0, 1))}
+        >
+
+            {children}
+        </Tippy>
+    );
+}
+Menu.propTypes = {
+    children: PropTypes.node.isRequired,
+    items: PropTypes.array,
+    onChange: PropTypes.func,
+    hideOnClick: PropTypes.bool
+}
+export default Menu;
---
+++ src/layouts/components/Search/Search.js
@@ -0,0 +1,113 @@
+import { useEffect, useState, useRef } from 'react';
+import HeadlessTippy from '@tippyjs/react/headless';
+import AccountItem from '~/components/AccountItem';
+import { Wrapper as PopperWrapper } from '~/components/Popper';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames/bind';
+import styles from './Search.module.scss';
+import { useDebounce } from '~/hooks';
+import { SearchIcon } from '~/components/Icons';
+import * as searchServices from '~/apiServices/searchServices';
+
+
+import {
+    faCircleXmark,
+    faSpinner,
+} from '@fortawesome/free-solid-svg-icons';
+const cx = classNames.bind(styles);
+
+
+function Search() {
+    const [searchValue, setSearchValue] = useState('');
+    const [searchResult, setSearchResult] = useState([]);
+    const [showResult, setShowResult] = useState(true);
+    const [loading, setLoading] = useState(true);
+    const debounced = useDebounce(searchValue, 500);
+    const inputRef = useRef();
+    const handleClear = () => {
+
+        setSearchValue('');
+        setSearchResult([]);
+        inputRef.current.focus();
+
+    }
+    const handleHideResult = () => {
+        setShowResult(false);
+    }
+    useEffect(() => {
+        if (!debounced.trim()) {
+            setSearchResult([]);
+            setLoading(false);
+            return;
+        }
+        setLoading(true);
+        const fetchApi = async () => {
+            setLoading(true);
+
+            const result = await searchServices.search(debounced);
+            setSearchResult(result);
+
+            setLoading(false);
+        }
+
+        fetchApi();
+    }, [debounced]);
+
+    const handleChange = (e) => {
+        const searchValue = e.target.value;
+        if (!searchValue.startsWith(' ')) {
+            setSearchValue(searchValue);
+        }
+
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
+    return (
+        <div>
+            <HeadlessTippy
+                interactive
+                visible={showResult && searchResult.length > 0}
+                render={(attrs) => (
+                    <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+                        <PopperWrapper>
+                            <h4 className={cx('search-title')}>Accounts</h4>
+                            {
+                                searchResult.map((result) => (
+                                    <AccountItem key={result.id} data={result} />
+                                ))
+                            }
+
+                        </PopperWrapper>
+                    </div>
+                )}
+                onClickOutside={handleHideResult}>
+                <div className={cx('search')}>
+                    <input
+                        ref={inputRef}
+                        value={searchValue}
+                        placeholder="Search accounts and videos"
+                        spellCheck={false}
+                        onChange={handleChange}
+                        onFocus={() => setShowResult(true)}
+                    />
+                    {!!searchValue && !loading && (
+                        <button className={cx('clear')}
+                            onClick={handleClear}
+                        >
+                            <FontAwesomeIcon icon={faCircleXmark} />
+                        </button>
+                    )}
+                    {loading && <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />}
+
+                    <button className={cx('search-btn')} onMouseDown={handleSubmit}>
+                        <SearchIcon />
+                    </button>
+                </div>
+            </HeadlessTippy >
+        </div>
+    );
+}
+
+export default Search;
